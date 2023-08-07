0e4a326064a29130c928b111b9ff271edf07270b
Mohamed Sayed <ms10596@gmail.com>
2023-08-07 16:37:26 +0300

    add PlotlyBase

--- app.jl
+++ app.jl
@@ -1,18 +1,46 @@
 module App
+using PlotlyBase
 using GenieFramework
 @genietools
 
-function count_vowels(message)
-    sum([c ∈ ['a', 'e', 'i', 'o', 'u'] for c in lowercase(message)])
-end
-
 @app begin
-    @in message = ""
-    @out vowels = 0
-    @onchange message begin
-        vowels = count_vowels(message)
+    @out trace = [
+        scattergeo(
+            locationmode="ISO-3",
+            lon=[-0.12, -74],
+            lat=[51.50, 40.71],
+            text=["London", "New York"],
+            textposition="bottom right",
+            textfont=attr(family="Arial Black", size=18, color="blue"),
+            mode="markers+text",
+            marker=attr(size=10, color="blue"),
+            name="Cities"),
+        scattergeo(
+            locationmode="ISO-3",
+            lon=[-0.12, -74],
+            lat=[51.50, 40.71],
+            mode="lines",
+            line=attr(width=2, color="red"),
+            name="Route")
+                 ]
+    @out layout = PlotlyBase.Layout(
+            title = "World Map",
+            showlegend = false,
+            geo = attr(
+                showframe = false,
+                showcoastlines = false,
+                projection = attr(type = "natural earth")
+            ))
+    @in data_click = Dict{String, Any}()  # data from map click event
+    @in data_cursor = Dict{String, Any}()
+
+    @onchange data_click begin
+        @show data_click
+        @show data_cursor
     end
 end
+
+@mounted watchplots()
 
 @page("/", "app.jl.html")
 end
