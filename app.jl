4f06e9b7d0834ac1acd7466c15bcb7b483021b78
Noran Raafat <noran.raafat@optomatica.com>
2023-08-10 17:05:27 +0300

    map csv fields lon and lat

--- app.jl
+++ app.jl
@@ -22,11 +22,24 @@ function myplot(args::Dict=Dict())
   scattermapbox(; Dict(default_scatter_args..., args...)...)
 end
 
+function mapFields(df::DataFrame)
+
+  latInd = findfirst(x -> occursin("lat", lowercase(x)), names(df))
+  lonInd = findfirst(x -> occursin("lon", lowercase(x)), names(df))
+
+  return DataFrame(
+    Longitude=df[!, lonInd],
+    Latitude=df[!, latInd],
+    Magnitude=fill(3, size(df, 1))
+  )
+end
+
 @app begin
   @in left_drawer_open = true
   @in current_year = 2023
   @in selected_color = "rgb(51, 153, 255)"
 
+  @out input_data = DataFrame()
   @out data = DataFrame(Longitude=[], Latitude=[], Magnitude=[])
   @out trace = [myplot()]
   @out layout = PlotlyBase.Layout(
@@ -41,7 +54,8 @@ end
       showcoastlines=false,
       projection=attr(type="natural earth")
     ))
-  @onchange data begin
+  @onchange input_data begin
+    model.data[] = mapFields(input_data)
     trace = [myplot(
       Dict(
         :lon => data[!, "Longitude"],
@@ -51,10 +65,12 @@ end
   end
 
   @onchange selected_color begin
+    model.data[] = mapFields(input_data)
+
     trace = [
       myplot(Dict(
         :marker => attr(
-          size=(data[!, "Magnitude"] .^ 3) ./ 20,
+          # size=(data[!, "Magnitude"] .^ 3) ./ 20,
           color=selected_color,
           line=attr(color="rgb(255, 255, 255)", width=0.5)
         ),
@@ -75,7 +91,7 @@ end
 route("/", method=POST) do
   files = Genie.Requests.filespayload()
   f = first(files)
-  model.data[] = CSV.read(f[2].data, DataFrame)
+  model.input_data[] = CSV.read(f[2].data, DataFrame)
   return "Perfecto!"
 end
 
--- ui.jl
+++ ui.jl
@@ -10,7 +10,7 @@ ui = () -> StippleUI.layout(
     ),
     drawer(
       [uploader(label="Upload Dataset", accept=".csv", method="POST", url="http://127.0.0.1:8000/", @on(:uploaded, :uploaded), style="width:200px"),
-        quasar(:color, var"v-model"=:selected_color, label="Color"),
+        item([itemsection(p("Marker color")), itemsection(input(type="color", var"v-model"=:selected_color, label="Color"))]),
         item([itemsection(btn(; dense=true, flat=true, round=true, icon="arrow_right"); avatar=true), itemsection(slider(1:5:100, :current_year))]),
         h1("{{current_year}}")
       ],
