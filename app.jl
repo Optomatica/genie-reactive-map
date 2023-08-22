ae07762924fd2c359b5a6bc478f54147a4d0e91f
Mohamed Sayed <ms10596@gmail.com>
2023-08-22 12:59:28 +0300

    replace token with ENV

--- app.jl
+++ app.jl
@@ -30,9 +30,7 @@ using .Utils: scale_array, map_fields
   @out max_year = current_year
   @out trace = [scattermapbox()]
   @out layout = PlotlyBase.Layout(margin=attr(l=0, r=0, t=0, b=0), mapbox=attr(style="open-street-map", zoom=1.7))
-  @out config = ConfigType(
-    "***REMOVED***"
-  )
+  @out config = ConfigType(ENV["MAPBOX_KEY"])
 
   @onchange data_input begin
     selected_size_feature = nothing
